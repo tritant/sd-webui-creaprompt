71e2feea7b13e503f3988ac35701bf10054375e3
tritant <tritant@hotmail.com>
2024-02-11 10:12:24 +0100

    update UI

--- scripts/creaprompt.py
+++ scripts/creaprompt.py
@@ -39,7 +39,7 @@ def read_random_line_from_csv_files(Prefix, sufix, *args):
         concatenated_lines = concatenated_lines + "," + sufix
     if not any(args):
         concatenated_lines = "Aucun résultat sélectionné."
-    return concatenated_lines    
+    return concatenated_lines
 
 class CreaPromptScript(scripts.Script):
     def __init__(self) -> None:
@@ -55,7 +55,7 @@ class CreaPromptScript(scripts.Script):
         with gr.Group():
             with gr.Accordion("CreaPrompt", open=False):
               gr.Markdown("# CreaPrompt")
-              gr.Markdown("Select the category you want to see in the prompt or build your prompt category by category")
+              gr.Markdown("Select the categories you want to use to create the prompt or build your prompt category by category")
               with gr.Row():
                 with gr.Column(scale=10):
                       checkboxes = []
@@ -66,25 +66,21 @@ class CreaPromptScript(scripts.Script):
                           checkboxes.append(checkbox) 
                           checkbox_rows = [[checkbox] for checkbox in checkboxes]
                 with gr.Column(scale=50):
-                      Prefix = grc.Textbox(
-                                            label="Prefix of the Prompt",
-                                            elem_id="promptgen_prompt_prefix",
-                                            show_label=True,
-                                            lines=2,
-                                            placeholder="Type your prefix or leave blank if you don't want it",
-                                            container=True
-                                           )
-                      #Prefix = gr.Textbox(label="Prefix of the Prompt", elem_id="promptgen_prompt_prefix", show_label=True, lines=2, placeholder="Type your prefix or leave blank if you don't want it").style(container=True)
-                      sufix = grc.Textbox(label="Suffix of the Prompt", elem_id="promptgen_prompt_sufix", show_label=True, lines=2, placeholder="Type your suffix or leave blank if you don't want it", container=True)
-                      prompt = grc.Textbox(label="Generated Prompt", elem_id="promptgen_prompt", show_label=True, lines=2, placeholder="Make the selection of your choice and Click Generate button", container=True)
+                      final = grc.Textbox(label="Final prompt which will be used to generate the image:", elem_id="promptgen_prompt_final", show_label=True, lines=2, placeholder="The final prompt is displayed here", container=True)
+                      Prefix = grc.Textbox(label="Prefix of the Prompt:", elem_id="promptgen_prompt_prefix", show_label=True, lines=2, placeholder="Type your prefix or leave blank if you don't want it", container=True)
+                      sufix = grc.Textbox(label="Suffix of the Prompt:", elem_id="promptgen_prompt_sufix", show_label=True, lines=2, placeholder="Type your suffix or leave blank if you don't want it", container=True)
+                      prompt = grc.Textbox(label="Created Prompt from categories:", elem_id="promptgen_prompt", show_label=True, lines=2, placeholder="Make the selection of your choice and Click Generate button", container=True)
                       with gr.Row():
-                            Sendbefore = gr.Button('Send before the prompt', elem_id="promptgen_sendto_img", variant='primary')
-                            Sendafter = gr.Button('Send after the prompt', elem_id="promptgen_sendto_txt", variant='primary')                            
-                      with gr.Column(scale=2): 
-                            send_text_button = gr.Button(value='Replace prompt', variant='primary')
-              with gr.Row():
-                    submit = gr.Button('Generate', elem_id="promptgen_generate", variant='primary')
-
+                            gr.Markdown("# Create prompt manually")
+                      with gr.Row():      
+                            gr.Markdown("Press the normal generate button to start generating image with the final prompt")
+                      with gr.Row():
+                            Sendbefore = gr.Button('Add before final prompt', elem_id="promptgen_sendto_img", variant='primary')
+                            send_text_button = gr.Button(value='Replace final prompt', variant='primary')
+                            Sendafter = gr.Button('Add after final prompt', elem_id="promptgen_sendto_txt", variant='primary')                            
+                      with gr.Row():
+                            submit = gr.Button('Create prompt from categories', elem_id="promptgen_generate", variant='primary')
+                
         with contextlib.suppress(AttributeError):
             submit.click(
                            fn=read_random_line_from_csv_files,
@@ -93,12 +89,18 @@ class CreaPromptScript(scripts.Script):
                           )        
             if is_img2img:
                 Sendbefore.click(fn=send_before_prompt, inputs=[prompt, self.boxxIMG], outputs=[self.boxxIMG])
+                Sendbefore.click(fn=send_before_prompt, inputs=[prompt, self.boxxIMG], outputs=[final])
                 Sendafter.click(fn=send_after_prompt, inputs=[prompt, self.boxxIMG], outputs=[self.boxxIMG])
+                Sendafter.click(fn=send_after_prompt, inputs=[prompt, self.boxxIMG], outputs=[final])
                 send_text_button.click(fn=send_text_to_prompt, inputs=[prompt, self.boxxIMG], outputs=[self.boxxIMG])
+                send_text_button.click(fn=send_text_to_prompt, inputs=[prompt, self.boxxIMG], outputs=[final])
             else:
                 Sendbefore.click(fn=send_before_prompt, inputs=[prompt, self.boxx], outputs=[self.boxx])
+                Sendbefore.click(fn=send_before_prompt, inputs=[prompt, self.boxx], outputs=[final])
                 Sendafter.click(fn=send_after_prompt, inputs=[prompt, self.boxx], outputs=[self.boxx])
+                Sendafter.click(fn=send_after_prompt, inputs=[prompt, self.boxx], outputs=[final])
                 send_text_button.click(fn=send_text_to_prompt, inputs=[prompt, self.boxx], outputs=[self.boxx])
+                send_text_button.click(fn=send_text_to_prompt, inputs=[prompt, self.boxx], outputs=[final])
         return [prompt, send_text_button]
 
     def after_component(self, component, **kwargs):
