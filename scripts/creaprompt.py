0b94f3fca517278964e184dad5df1316ed59b3d0
tritant <tritant@hotmail.com>
2024-03-07 14:33:58 +0100

    update UI

--- scripts/creaprompt.py
+++ scripts/creaprompt.py
@@ -144,15 +144,15 @@ class CreaPromptScript(scripts.Script):
         #is_enabled=""
         with gr.Group():
             with gr.Accordion("CreaPrompt : Not Active",open = False) as acc:
-              with gr.Accordion("1----CreaPrompt collection", open=True):
-                     gr.Markdown("# CreaPrompt collection")
+              with gr.Accordion("1--CreaPrompt collection", open=True):
+                     #gr.Markdown("# CreaPrompt collection")
                      gr.Markdown("When activated, just press the normal generate button, it also works with batch")
                      with gr.Row():
                        is_collection_enabled = grc.Checkbox(label="Enable auto prompting", info="From CreaPrompt collection", value=False)
                        is_randomize_manu = grc.Checkbox(label="Enable random prompts", info="For each images in batch", value=False, interactive=True)
-              with gr.Accordion("2----Auto prompting from categories", open=False):
+              with gr.Accordion("2--Auto prompting from categories", open=False):
                      with gr.Column(scale=3):
-                       gr.Markdown("# Auto prompting from categories")
+                       #gr.Markdown("# Auto prompting from categories")
                        gr.Markdown("When activated, just press the normal generate button, it also works with batch")
                      with gr.Row():
                        is_enabled = grc.Checkbox(label="Enable auto prompting", info="From selected categories", value=False)
@@ -173,9 +173,9 @@ class CreaPromptScript(scripts.Script):
                        save_state_button = gr.Button("Save your preset categories", elem_id="save_state", variant="primary")
                        file_name_textbox = grc.Textbox(elem_id="file_name", show_label=False, placeholder="Enter your preset name", container=True)
                        file_dropdown_component = gr.Dropdown(show_label=False, choices=get_config_files(), elem_id="file_dropdown", value="Select a preset")
-              with gr.Accordion("3----Create prompt manually from categories", open=False):         
+              with gr.Accordion("3--Create prompt manually from categories", open=False):         
                      with gr.Column(scale=3):
-                       gr.Markdown("# Create prompt manually from categories")
+                       #gr.Markdown("# Create prompt manually from categories")
                        gr.Markdown("Press the normal generate button to start generating image with the final prompt")
                        final = grc.Textbox(label="Final prompt which will be used to generate the image:", elem_id="creaprompt_prompt_final", show_label=True, lines=2, placeholder="The final prompt is displayed here", container=True)
                        Prefix = grc.Textbox(label="Prefix of the Prompt:", elem_id="prompt_prefix", show_label=True, lines=2, placeholder="Type your prefix or leave blank if you don't want it", container=True)
